2dd04b95b6740838f37cf9b6b11fc52aebaa709e
Boris Mokriev <borismokriev@gmail.com>
2022-04-27 18:38:52 +0300

    Set Roboto as main font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
 };
