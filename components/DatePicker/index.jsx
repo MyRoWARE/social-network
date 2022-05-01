3088027a5cdbc6241668df66e7a498ff2bcdbe42
Federico <fedeacos045@gmail.com>
2022-04-30 21:50:52 -0300

    added input classes

--- components/DatePicker/index.jsx
+++ components/DatePicker/index.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 export const DatePicker = () => {
   return (
-    <>
-      <h1>select date birth</h1>
-      <input type="date" />
-    </>
+    <div className="w-full">
+      <div className="flex gap-2 p-2 rounded-xl border-2 border-gray-400">
+        <h1>select date birth</h1>
+        <input type="date" />
+      </div>
+    </div>
   );
 };
