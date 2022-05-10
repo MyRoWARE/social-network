4502e81ffb93a8bb7c7e00aa2ba5150ac32c61e2
Matt <60991158+matthewfee@users.noreply.github.com>
2022-05-10 15:58:09 -0600

    added login page

---
+++ stories/login.stories.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+import Login from '../pages/login'
+
+export default {
+  title: 'Pages/Login',
+  component: Login
+}
+
+export const LoginPage = () => <Login />
