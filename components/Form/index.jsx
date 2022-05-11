c24789856bf43bb5994db57dbf2951c0d9b5441c
MyRoWARE <myroware@gmail.com>
2022-05-12 00:17:17 +0200

    Added Gender Input component & added it to Form

--- components/Form/index.jsx
+++ components/Form/index.jsx
@@ -1,32 +1,35 @@
-import React from 'react'
-import { TextInput } from '../NameInput'
-import { GoogleExternalSignup, AppleExternalSignup } from './ExternalSignup'
-import { Button } from '../Button'
-import { PasswordInput } from '../PasswordInput'
+import React from "react";
+import { TextInput } from "../NameInput";
+import { GoogleExternalSignup, AppleExternalSignup } from "./ExternalSignup";
+import { Button } from "../Button";
+import { PasswordInput } from "../PasswordInput";
+import { GenderInput } from "../GenderInput";
 
 export const Form = ({ login, signup }) => {
   return (
-    <form className='bg-white rounded-lg p-5 mx-4 md:mx-auto flex flex-col justify-between gap-5 drop-shadow-xl'>
-      <div className='signup-with flex justify-between'>
+    <form className="bg-white rounded-lg p-5 mx-4 md:mx-auto flex flex-col justify-between gap-5 drop-shadow-xl">
+      <div className="signup-with flex justify-between">
         <GoogleExternalSignup />
         <AppleExternalSignup />
       </div>
 
-      <div className='relative flex py-2 items-center'>
-        <div className='flex-grow border-t border-gray-300'></div>
-        <span className='flex-shrink mx-4 text-gray-600'>OR</span>
-        <div className='flex-grow border-t border-gray-300'></div>
+      <div className="relative flex py-2 items-center">
+        <div className="flex-grow border-t border-gray-300"></div>
+        <span className="flex-shrink mx-4 text-gray-600">OR</span>
+        <div className="flex-grow border-t border-gray-300"></div>
       </div>
 
-      <TextInput type='name' />
-      {signup && <TextInput type='email' />}
+      <TextInput type="name" />
+      {signup && <TextInput type="email" />}
+
+      {signup && <GenderInput />}
 
       {signup && <Button>Sign Up</Button>}
 
       {signup && (
-        <small className='mx-auto'>
+        <small className="mx-auto">
           {`Already have an account?`}
-          <a className='text-blue-500 hover:text-blue-700' href='#'>
+          <a className="text-blue-500 hover:text-blue-700" href="#">
             Sign In
           </a>
         </small>
@@ -36,13 +39,13 @@ export const Form = ({ login, signup }) => {
       {login && <Button>Login</Button>}
 
       {login && (
-        <small className='mx-auto'>
+        <small className="mx-auto">
           {`Don't have an account?  `}
-          <a className='text-blue-500 hover:text-blue-700' href='#'>
+          <a className="text-blue-500 hover:text-blue-700" href="#">
             Sign Up
           </a>
         </small>
       )}
     </form>
-  )
-}
+  );
+};
---
+++ components/GenderInput/index.jsx
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from "react";
+import "./style.css";
+
+export const GenderInput = ({ inputChange, error }) => {
+  const [gender, setGender] = useState("");
+
+  return (
+    <div className="w-2/4">
+      <div className="flex gap-2 p-2 rounded-xl border-2 border-gray-400 h-12">
+        {gender === "female" ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-venus"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#2c3e50"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <circle cx="12" cy="9" r="5" />
+            <line x1="12" y1="14" x2="12" y2="21" />
+            <line x1="9" y1="18" x2="15" y2="18" />
+          </svg>
+        ) : gender === "male" ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-mars"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="#2c3e50"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <circle cx="10" cy="14" r="5" />
+            <line x1="19" y1="5" x2="13.6" y2="10.4" />
+            <line x1="19" y1="5" x2="14" y2="5" />
+            <line x1="19" y1="5" x2="19" y2="10" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="icon icon-tabler icon-tabler-question-mark"
+            viewBox="0 0 24 24"
+            strokWidth="1.5"
+            stroke="#2c3e50"
+            fill="none"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M8 8a3.5 3 0 0 1 3.5 -3h1a3.5 3 0 0 1 3.5 3a3 3 0 0 1 -2 3a3 4 0 0 0 -2 4" />
+            <line x1="12" y1="19" x2="12" y2="19.01" />
+          </svg>
+        )}
+
+        <div className="form-check form-check-inline pl-2">
+          <input
+            className="form-check-input form-check-input rounded-full h-4 w-4 border border-black bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+            type="radio"
+            name="gender"
+            id="male"
+            value="male"
+            onChange={(e) => setGender(e.target.value)}
+          />
+          <label className="form-check-label inline-block" htmlFor="optioMale">
+            Male
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input form-check-input rounded-full h-4 w-4 border border-black bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+            type="radio"
+            name="gender"
+            id="female"
+            value="female"
+            onChange={(e) => setGender(e.target.value)}
+          />
+          <label
+            className="form-check-label inline-block"
+            htmlFor="optionFemale"
+          >
+            Female
+          </label>
+        </div>
+      </div>
+
+      <p
+        style={{
+          color: "#FF5630",
+          display: error ? "block" : "none",
+        }}
+      >
+        Please enter a valid Name
+      </p>
+    </div>
+  );
+};
