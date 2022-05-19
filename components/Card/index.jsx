3293d379298e7726b1b1b65760555b91820da527
Matt <60991158+matthewfee@users.noreply.github.com>
2022-05-19 11:14:19 -0600

    added feed page with create post and navbar. Changed container layout from absolute positioning to relative

--- components/Card/index.jsx
+++ components/Card/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Card = ({ children, height }) => {
   return (
     <div
-      className='bg-white mt-24 rounded-lg p-5 
+      className='bg-white mt-24 rounded-lg p-1 
     mx-4 md:mx-auto flex 
     flex-col justify-between 
     gap-5 drop-shadow-xl
--- components/CreatePost/index.jsx
+++ components/CreatePost/index.jsx
@@ -10,19 +10,13 @@ export const CreatePost = () => {
 
   return (
     <div
-      className='bg-white mt-24 md:rounded-lg p-5 
+      className='bg-white mt-8 md:rounded-lg p-5 
     mx-4 md:mx-auto flex 
     flex-col justify-between 
     gap-5 drop-shadow-xl
     w-full max-w-[512px]
-    h-auto text-gray-600'
+    h-auto text-gray-600 my-0'
     >
-      {/* <PostHeader
-        headerImageSRC={
-          'photo-1651772688322-a4c82677a043?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8'
-        }
-      /> */}
-
       <div className='flex justify-between items-center'>
         <PostProfileImage imageSRC={headerImageSRC} />
         <PostInput />
