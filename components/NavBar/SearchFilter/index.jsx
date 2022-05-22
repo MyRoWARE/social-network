61c07a7bc907eb20e7b545a37e38a887ddccc1be
Federico <fedeacos045@gmail.com>
2022-05-22 11:42:22 -0300

    styles for mobile

--- components/NavBar/SearchFilter/index.jsx
+++ components/NavBar/SearchFilter/index.jsx
@@ -6,27 +6,35 @@ export const SearchFilter = () => {
   const handleOnChange = (event) => {
     const { value } = event.target;
     setInputValues({ ...inputValues, value });
+    console.log(event.target.value);
   };
 
   return (
-    <div>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        x="0px"
-        y="0px"
-        width="50"
-        height="50"
-        viewBox="0 0 50 50"
-      >
-        <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
-      </svg>
-      <input
-        className="flex gap-2 p-2 rounded-xl border-2 border-none h-12 outline-none pl-4 bg-[#4E5D780D] w-full ml-4"
-        type="text"
-        placeholder="Search for something here..."
-        required
-        onChange={handleOnChange}
-      />
+    <div className="">
+      <div className="w-full relative flex flex-row">
+        <button
+          type="submit"
+          className="w-full h-15 border-2 border-solid border-slate-500 text-center rounded-md flex flex-row"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="20"
+            height="30"
+            viewBox="0 0 50 50"
+          >
+            <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
+          </svg>
+          <input
+            className="flex  p-2  border-none outline-none  w-full text-[10px]"
+            type="text"
+            placeholder="Search for something here..."
+            required
+            onChange={handleOnChange}
+          />
+        </button>
+      </div>
     </div>
   );
 };
--- components/NavBar/index.jsx
+++ components/NavBar/index.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { PostProfileImage } from "../PostCard/PostProfileImage/index";
+import { Notifications } from "./Notifications";
 import { SearchFilter } from "./SearchFilter";
 
 export const NavBar = ({ children }) => {
   return (
     <div
-      className="bg-white mt-24 md:rounded-lg p-5 
+      className="bg-white mt-24  p-5 
     mx-4 md:mx-auto flex 
-    flex-col justify-between 
-    gap-5 drop-shadow-xl
-    w-full max-w-[512px]
-    h-auto text-gray-600"
+    flex-row justify-around  
+    drop-shadow-xl
+    h-auto text-gray-600 sticky top-0 "
     >
       <PostProfileImage imageSRC="photo-1560250097-0b93528c311a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fHByb2ZpbGV8ZW58MHx8MHx8" />
       <SearchFilter />
+      <Notifications />
     </div>
   );
 };
