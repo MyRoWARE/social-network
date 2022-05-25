395d02b806923f59f3ceb3a460b5b64618013666
Saka Ricky <sakaricky91@gmail.com>
2022-05-25 13:59:18 +0100

    feat: added post card with backdrop

--- components/PostInput/index.jsx
+++ components/PostInput/index.jsx
@@ -1,9 +1,13 @@
-export const PostInput = ({ inputChange }) => (
-  <input
-    className="flex gap-2 p-2 rounded-xl border-2 border-none h-12 outline-none pl-4 bg-[#4E5D780D] w-full ml-4"
-    type="text"
-    placeholder="What's happening?"
-    required
-    onChange={inputChange}
-  />
-);
+export const PostInput = ({ inputChange, size }) => {
+  const padding = size === 'small' ? '2' : '32';
+
+  return (
+    <input
+      className={`flex gap-2 w-full ml-4 p-2 pb-${padding} pt-4 rounded-xl border-2 border-none h-12 outline-none bg-[#4E5D780D]`}
+      type="text"
+      placeholder="What's happening?"
+      required
+      onChange={inputChange}
+    />
+  );
+};
