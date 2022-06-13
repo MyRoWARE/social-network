6d1cc6c4023ac8064cfc555a5eb3c297a8107bcb
Federico <fedeacos045@gmail.com>
2022-06-13 18:51:21 -0300

    fixed profile img, changed sidebar for logo

--- components/NavBar/SearchFilter/index.jsx
+++ components/NavBar/SearchFilter/index.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 export const SearchFilter = () => {
-  const [inputValues, setInputValues] = useState("");
+  const [inputValues, setInputValues] = useState('');
 
   const handleOnChange = (event) => {
     const { value } = event.target;
@@ -27,7 +27,7 @@ export const SearchFilter = () => {
             <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
           </svg>
           <input
-            className="flex  p-2  border-none outline-none  w-full text-[10px]"
+            className="flex  p-2  border-none outline-none  w-full text-[14px]"
             type="text"
             placeholder="Search for something here..."
             required
--- components/NavBar/index.jsx
+++ components/NavBar/index.jsx
@@ -1,31 +1,53 @@
 import React from 'react';
-import { PostProfileImage } from '../PostCard/PostProfileImage/index';
+import { MyImage } from '../MyImage';
 import { Notifications } from './Notifications';
 import { SearchFilter } from './SearchFilter';
-import { Sidebar } from '../Sidebar';
 
+const imageSRC =
+  'photo-1560250097-0b93528c311a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fHByb2ZpbGV8ZW58MHx8MHx8';
 export const NavBar = ({ children }) => (
   <div>
     <div
       className="bg-white mt-24  p-4 
-     mx-auto flex 
-    sm:flex-row
-    lg:flex-row-reverse
-    justify-around  
-  
-    drop-shadow-xl
-    h-auto text-gray-600 sticky top-0 "
+      relative
+      w-full
+      h-full
+      flex 
+      items-center
+      lg:flex-row-reverse
+      justify-around  
+      drop-shadow-xl"
     >
-      <PostProfileImage
-        imageSRC="photo-1560250097-0b93528c311a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjV8fHByb2ZpbGV8ZW58MHx8MHx8"
-        className="rounded-md"
-      />
+      <div className="flex lg:flex-row-reverse ">
+        <div className="rounded-lg overflow-hidden h-12 w-12 relative cursor-pointer">
+          <MyImage src={imageSRC} width={60} height={60} alt="Profile picture" />
+        </div>
+        <div className="hidden  lg:block lg:flex items-center lg:whitespace-nowrap lg:px-4  ">
+          <h3 className="text-gray-600 lg:align-middle">John Cenna</h3>
+        </div>
+      </div>
       <SearchFilter />
       <div className="flex lg:hidden ">
         <Notifications />
       </div>
-      <div className="hidden sm:hidden md:hidden lg:contents">
-        <Sidebar />
+      <div className=" hidden lg:flex lg:align-top lg:contents logo text-[#4E5D78] font-bold text-2xl flex">
+        <div className="logo-title ml-2.5">Meetmax</div>
+        <svg
+          width="26"
+          height="26"
+          viewBox="0 0 26 26"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M10.4 0H15.6C18.3583 0 21.0035 1.12381 22.9539 3.12419C24.9043 5.12458 26 7.83769 26 10.6667C26 13.4956 24.9043 16.2088 22.9539 18.2091C21.0035 20.2095 18.3583 21.3333 15.6 21.3333V26C9.1 23.3333 0 19.3333 0 10.6667C0 7.83769 1.09571 5.12458 3.04609 3.12419C4.99647 1.12381 7.64175 0 10.4 0V0Z"
+            fill="#377DFF"
+          />
+          <path
+            d="M19 9C19 10.5913 18.3679 12.1174 17.2426 13.2426C16.1174 14.3679 14.5913 15 13 15C11.4087 15 9.88258 14.3679 8.75736 13.2426C7.63214 12.1174 7 10.5913 7 9L9.4 9C9.4 9.95478 9.77928 10.8705 10.4544 11.5456C11.1295 12.2207 12.0452 12.6 13 12.6C13.9548 12.6 14.8705 12.2207 15.5456 11.5456C16.2207 10.8705 16.6 9.95478 16.6 9H19Z"
+            fill="white"
+          />
+        </svg>
       </div>
     </div>
   </div>
